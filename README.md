050c4f599188c2b6cda99215276d1a82aeb4b273
russ <russell.winch@gmail.com>
2019-07-11 23:06:02 +0100

    Update setup script and readme.
    Add git prompt colors symlink.
    Update readme about bash-git-prompt install.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Dotfiles for Bash, Neovim, pdb, tmux
 
 Clone into home directory ~/
 Setup.sh should set up symbolic links after cloning the repo.
+
+Install bash-git-prompt from: https://github.com/magicmonty/bash-git-prompt
--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 mkdir -p ~/.config/nvim
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/bash/git-prompt-colors.sh ~/.git-prompt-colors.sh 
 source ~/.bash_profile
