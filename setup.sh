21a017a9c4751b2eaca4615f114172f214579656
russ <russell.winch@gmail.com>
2019-08-14 07:17:31 +0100

    Add fish config
    setup.sh updated with symbolic link.
    Customised fish prompt.
    Added date and full pwd listing.

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ mkdir -p ~/.config/nvim
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/bash/git-prompt-colors.sh ~/.git-prompt-colors.sh 
+ln -s ~/.dotfiles/fish/ ../.config/fish
 source ~/.bash_profile
