bf4f98617c01727d369779455c5fa4f4e62989b7
russ <russell.winch@gmail.com>
2019-08-18 19:21:02 +0100

    Prevent ctrl-d from exiting fish shell
    Will only be triggered on the fifth keystroke.

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,12 @@ set -gx EDITOR nvim
 # fzf should use the silver searcher
 set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
 
+# prevent ctrl-d exiting until the 5th time
+function fish_user_key_bindings
+    bind \cd delete-char
+    bind \cd\cd\cd\cd\cd exit
+end
+
 # pyenv
 set -x PATH "$HOME/.pyenv/bin:$PATH"
 pyenv init - | source
